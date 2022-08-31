657b3a293d1eb76bb962d0e43aa634fe2bad0da7
srikar-samudrala <srikar@eve.world>
2022-08-31 13:10:21 +0530

    adds frontend interaction

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -23,7 +23,7 @@ const networkConfig: {
 };
 
 const developmentChains = ["hardhat", "localhost"];
-const DECIMALS = "18";
-const INITIAL_ANSWER = "2000000000000000000000";
+const DECIMALS = "8";
+const INITIAL_ANSWER = "200000000000";
 
 export { networkConfig, developmentChains, DECIMALS, INITIAL_ANSWER };
